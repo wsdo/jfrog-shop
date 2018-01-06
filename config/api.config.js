ef9379ebd17c04c7917c235b6ff2254fbc228090
wangshudong <wsd312@163.com>
2018-01-07 00:13:08 +0800

    init

--- config/api.config.js
+++ config/api.config.js
@@ -1,4 +1,4 @@
 const isProdMode = Object.is(process.env.NODE_ENV, 'production')
 module.exports = {
-  baseUrl: isProdMode ? 'http://ifrog.shudong.wang/api/' : 'api/'
+  baseUrl: isProdMode ? 'http://jfrog.shudong.wang/api/' : 'api/'
 }
